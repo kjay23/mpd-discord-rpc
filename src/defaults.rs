206f3aca6052252cac0820cc709785f77c298163
Jan Leigh <therealkizu@protonmail.com>
2021-07-11 20:40:17 +0800

    feat: my version

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,9 +1,9 @@
 pub const IDLE_TIME: u64 = 5;
 pub const ACTIVE_TIME: u64 = 1;
 
-pub const DISCORD_ID: u64 = 677226551607033903;
+pub const DISCORD_ID: u64 = 861914207389089822;
 pub const DEFAULT_HOST: &str = "localhost:6600";
 
 pub const DETAILS_FORMAT: &str = "$title";
-pub const STATE_FORMAT: &str = "$artist / $album";
-pub const TIMESTAMP_MODE: &str = "elapsed";
+pub const STATE_FORMAT: &str = "$artist";
+pub const TIMESTAMP_MODE: &str = "left";
